b30cf36fe57502a71eed07c492a27a9d89f346f8
Pavel <impetus@yandex.ru>
2023-03-20 13:38:30 +0300

    Add task 5.3

--- app/src/main/java/com/example/myapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -1,43 +1,54 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sharedPreferences: SharedPreferences
+    private var pressureSensor: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
-            putInt("key1",10000)
-            apply()
-        }
-        Log.d("RRR",sharedPref.getInt("key1",0).toString())
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE) ?: return
+    }
 
-
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val map = sharedPreferences.all
-        map.forEach { item ->
-            Log.d("RRR", "${item.key} => ${item.value}")
+    override fun onResume() {
+        super.onResume()
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+        if(pressureSensor != null) {
+            sensorManager.registerListener(this, pressureSensor, SensorManager.SENSOR_DELAY_GAME)
         }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event!!.sensor.type == pressureSensor!!.type) {
+            with (sharedPreferences.edit()) {
+                putFloat("pressure", event.values[0])
+                apply()
+            }
+        }
+        val pressurevalue = sharedPreferences.getFloat("pressure",0f)
+        findViewById<TextView>(R.id.textView).text = pressurevalue.toString()
 
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
     }
+
+
 }
